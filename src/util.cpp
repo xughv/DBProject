c6d8d47b62415bec5c5a3a75b342568f2717d754
Xughv <xughv@foxmail.com>
2016-06-06 16:47:37 +0800

    Merge branch 'Finn'
    # Conflicts:
    #	src/util.cpp
    #	src/util.h

--- src/util.cpp
+++ src/util.cpp
@@ -2,7 +2,9 @@
 // Created by 徐广晖 on 16/6/5.
 //
 
+
 #include "util.h"
+
 #include <cstdlib>
 #include <cmath>
 
@@ -13,5 +15,39 @@ float Rand() {
     float x = sqrt(-2.0f * log(u1)) * cos(2.0f * PI * u2);
 
     return x;
+}
+
+
+void ReadSetFromFile(char* file_name, int num, int dim, unsigned** datas) {
+    int i = 0;
+    int j = 0;
+    FILE* fp = NULL;
+
+    fp = fopen(file_name, "rb");			// open data file
+    if (!fp) {
+        printf("I could not open %s.\n", file_name);
+        return;
+    }
+
+    fseek(fp, 16, SEEK_SET);
+    i = 0;
+
+    while (!feof(fp) && i < num) {	// read data file
+        for (j = 0; j < dim; j++) {
+            fread(&datas[i][j], sizeof(char), 1, fp);
+        }
+        i++;
+    }
+    if (!feof(fp) && i == num) {		// check the size of set
+        printf("The size of set is larger than you input\n", false);
+    }
+    else if (feof(fp) && i < num) {
+        printf("Set the size of dataset to be %d. ", i);
+        printf("And try again\n", true);
+    }
+
+
+    fclose(fp);						// close data file
+    return;
 }
 
--- src/util.h
+++ src/util.h
@@ -7,7 +7,7 @@
 
 float Rand();
 
-bool ReadSetFromFile(char* file_name, int &num, int &dim, float** datas);
+void ReadSetFromFile(char* file_name, int num, int dim, float** datas);
 
 
 #endif // _UTIL_H_
