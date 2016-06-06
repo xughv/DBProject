e44fc7e5b78e17fc2c4d916b3af580b01f9dcfb9
57Finn <wu7finn@gmail.com>
2016-06-06 10:26:30 +0800

    ReadFromFile
    有个bug，一开始的两个数读不进来，但其他数的位置都是对的。

--- src/util.cpp
+++ src/util.cpp
@@ -2,4 +2,38 @@
 // Created by 徐广晖 on 16/6/5.
 //
 
+
 #include "util.h"
+
+void ReadSetFromFile(char* file_name, int num, int dim, unsigned** datas) {
+    int i = 0;
+    int j = 0;
+    FILE* fp = NULL;
+
+    fp = fopen(file_name, "rb");			// open data file
+    if (!fp) {
+        printf("I could not open %s.\n", file_name);
+        return;
+    }
+
+    fseek(fp, 16, SEEK_SET);
+    i = 0;
+
+    while (!feof(fp) && i < num) {	// read data file
+        for (j = 0; j < dim; j++) {
+            fread(&datas[i][j], sizeof(char), 1, fp);
+        }
+        i++;
+    }
+    if (!feof(fp) && i == num) {		// check the size of set
+        printf("The size of set is larger than you input\n", false);
+    }
+    else if (feof(fp) && i < num) {
+        printf("Set the size of dataset to be %d. ", i);
+        printf("And try again\n", true);
+    }
+
+
+    fclose(fp);						// close data file
+    return;
+}
