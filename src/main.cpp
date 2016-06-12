8e277f99d680465eb64bce151122a639144d8b7e
57Finn <wu7finn@gmail.com>
2016-06-12 18:51:35 +0800

    Main

--- src/main.cpp
+++ src/main.cpp
@@ -18,55 +18,45 @@ int main(int argc, char* argv[]) {
 
     strcpy(dir, OUTPUT_DIR_DEFAULT); // 默认输出文件夹
 
-
-    bool failed = false;
     int  cnt = 1;
 
-    while (cnt < argc && !failed) {
+    while (cnt < argc) {
         if (strcmp(argv[cnt], "-n") == 0) {
             num_data = atoi(argv[++cnt]);
-            printf("num_data = %d\n", num_data);
-            if (num_data <= 0) {
-                failed = true;
-                break;
-            }
+            printf("Data Num    = %d\n", num_data);
         }
         else if (strcmp(argv[cnt], "-d") == 0) {
             dim = atoi(argv[++cnt]);
-            printf("dim = %d\n", dim);
-            if (dim <= 0) {
-                failed = true;
-                break;
-            }
+            printf("Dim         = %d\n", dim);
         }
         else if (strcmp(argv[cnt], "-qn") == 0) {
             num_query = atoi(argv[++cnt]);
-            printf("num_query = %d\n", num_query);
-            if (num_query <= 0) {
-                failed = true;
-                break;
-            }
+            printf("Query Num   = %d\n", num_query);
         }
         else if (strcmp(argv[cnt], "-ds") == 0) {
             strncpy(data_set, argv[++cnt], sizeof(data_set));
-            printf("data set = %s\n", data_set);
+            printf("Data Set    = %s\n", data_set);
         }
         else if (strcmp(argv[cnt], "-qs") == 0) {
             strncpy(query_set, argv[++cnt], sizeof(query_set));
-            printf("query set = %s\n", query_set);
+            printf("Query Set   = %s\n", query_set);
         }
         else if (strcmp(argv[cnt], "-of") == 0) {
             strncpy(dir, argv[++cnt], sizeof(dir));
-            printf("output dir = %s\n", dir);
+            printf("Output Dir  = %s\n", dir);
         }
         else {
-            failed = true;
-            printf("Parameters error!\n");
-            break;
+            printf("Parameters error! Some parameters are not exist. \n");
+            printf("Please entry these parameters: -n -d -qn -ds -qs \n");
+            return 0;
         }
         cnt++;
     }
-    printf("\n");
+
+    if (num_data <= 0 || dim <= 0 || num_query <= 0) {
+        printf("Parameters error! Some parameters are missed or the value is smaller than zero\n");
+        return 0;
+    }
 
     int* result_id = new int[num_query];
 
@@ -74,9 +64,7 @@ int main(int argc, char* argv[]) {
     CalcANN(num_query, dim, query_set, dir, result_id);
     LinearScan(num_data, num_query, dim, query_set, data_set, result_id);
 
-//    delete[] data_set;
-//    delete[] query_set;
-//    delete[] dir;
+
     delete[] result_id;
     return 0;
 }
